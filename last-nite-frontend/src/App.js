ae2d05abf448b1f47c2cfec3c6eae5a3a1a47351
aloporsan <aloporsan@alum.us.es>
2025-09-12 09:50:20 +0200

    Pequeña prueba del fronetnd. Pestañas y arranque al sitio adecuado hecho

--- last-nite-frontend/src/App.js
+++ last-nite-frontend/src/App.js
@@ -1,27 +1,12 @@
 import './App.css';
-import AppNavBar from './components/appNavBar';
-import logo from './logo.svg';
+import AppNavBar from './components/AppNavBar/appNavBar';
+import Home from './components/Home/Home';
 
 function App() {
   return (
-    
     <div className="App">
-      <AppNavBar></AppNavBar>
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      
+      <AppNavBar />
+      <Home />
     </div>
   );
 }
